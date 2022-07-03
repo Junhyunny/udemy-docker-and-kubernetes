8ef325b903e5805f2d9b635586cf7c550083d480
Junhyunny <kang3966@naver.com>
2022-07-03 18:29:05 +0900

    Docs Dockerfile 변경 및 nodemon 사용

--- projects/2022-07-03-multi-01-starting-setup/backend/app.js
+++ projects/2022-07-03-multi-01-starting-setup/backend/app.js
@@ -84,8 +84,7 @@ app.delete('/goals/:id', async (req, res) => {
 });
 
 mongoose.connect(
-  // 'mongodb://mongodb:27017/course-goals',
-  'mongodb://mongoadmin:secret@mongodb:27017/course-goals?authSource=admin',
+  `mongodb://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@mongodb:27017/course-goals?authSource=admin`,
   {
     useNewUrlParser: true,
     useUnifiedTopology: true
